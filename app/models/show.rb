ef278b80c2db843efb6a7e9880d9c93d6f3edfcf
Tracy Reuther <tracyreuther@gmail.com>
2019-08-23 18:33:09 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,8 +3,8 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   belongs_to :network
 
-  # def actors_list
-  #   character_name = self.characters.map {|character| character.actor}
-  #   character_name.Actor.full_name
-  # end
+  def actors_list
+    # character_name = self.characters.map {|character| character.actor}
+    # character_name.Actor.full_name
+  end
 end
